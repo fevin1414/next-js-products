e7fa570c430d765a7e74a27ffaeb8a10d464d510
fevin1414 <fevinbiju1414@gmail.com>
2023-09-08 13:26:38 -0500

    added ISR

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
  const jsonData=await fs.readFile(filePath);
  const data=JSON.parse(jsonData);
-  return { props: { products: data.products }};
+  return { props: { products: data.products },
+  revalidate:10
+};
 }
 export default HomePage;
