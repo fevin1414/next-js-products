ae6c0a7d9678aa72893dc973b296617f7065d704
fevin1414 <fevinbiju1414@gmail.com>
2023-09-08 09:38:46 -0500

    added get staticProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 function HomePage(props) {
-  return (
 
+  const {products} = props;
+  return (
+<ul>
+  {products.map(product => <li key={product.id}>{product.title}</li>)}
+</ul>
   );
 }
 
+export async function getStaticProps(){
+  return {props:{products:[{id:'p1',title:'product 1'}],}};
+}
 export default HomePage;
